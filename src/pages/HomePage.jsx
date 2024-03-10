1a9b0e01f70edbaee75ce89fe3ebb4f5c3e86421
vky5 <beta@blacktree.comnect.local>
2024-03-10 13:20:02 +0530

    tailwind configured

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,16 @@
 function HomePage() {
   return (
     <div>
-      
+      <div>
+      <div className="flex flex-col justify-center absolute w-full h-screen z-10">
+            <div className="">
+                <h1 className="font-samarkan text-white text-[320px] text-center">SAC</h1>
+            </div>    
+                <div className="flex justify-end pr-[186px] text-2xl">
+                    <h1 className="font-cM text-white  ">Student Activity Cell</h1>
+                </div>
+            </div>
+      </div>
     </div>
   )
 }

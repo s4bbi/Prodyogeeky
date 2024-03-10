9844be67aabac3326c3c68e01a51634541a44006
Zeno7081205 <zenotron96@gmail.com>
2024-03-10 13:49:49 +0530

    rohit commit

---
+++ src/data/slideshow.js
@@ -0,0 +1,99 @@
+import img1 from '../images/carousel/1.jpg';
+import img2 from '../images/carousel/2.jpg';
+import img3 from '../images/carousel/3.jpg';
+import img4 from '../images/carousel/4.jpg';
+import img5 from '../images/carousel/5.jpg';
+import img6 from '../images/carousel/6.jpg';
+import img7 from '../images/carousel/7.jpg';
+import img8 from '../images/carousel/8.jpg';
+import img9 from '../images/carousel/9.jpg';
+import img10 from '../images/carousel/10.jpg';
+import img11 from '../images/carousel/11.jpg';
+import img12 from '../images/carousel/12.jpg';
+import img13 from '../images/carousel/13.jpg';
+
+
+const imgSlide = [
+    {
+      id: 1,
+      src: img1,
+      alt: "Image 1",
+      caption: "Image 1 Description",
+    },
+    {
+      id: 2,
+      src: img2,
+      alt: "Image 2",
+      caption: "Image 2 Description",
+    },
+    {
+      id: 3,
+      src: img3,
+      alt: "Image 3",
+      caption: "Image 3 Description",
+    },
+    {
+      id: 4,
+      src: img4,
+      alt: "Image 4",
+      caption: "Image 4 Description",
+    },
+    {
+      id: 5,
+      src: img5,
+      alt: "Image 5",
+      caption: "Image 5 Description",
+    },
+    {
+      id: 6,
+      src: img6,
+      alt: "Image 6",
+      caption: "Image 6 Description",
+    },
+    {
+      id: 7,
+      src: img7,
+      alt: "Image 7",
+      caption: "Image 7 Description",
+    },
+    {
+      id: 8,
+      src: img8,
+      alt: "Image 8",
+      caption: "Image 8 Description",
+    },
+    {
+      id: 9,
+      src: img9,
+      alt: "Image 9",
+      caption: "Image 9 Description",
+    },
+    {
+      id: 10,
+      src: img10,
+      alt: "Image 10",
+      caption: "Image 10 Description",
+    },
+    {
+      id: 11,
+      src: img11,
+      alt: "Image 11",
+      caption: "Image 11 Description",
+    },
+    {
+      id: 12,
+      src: img12,
+      alt: "Image 12",
+      caption: "Image 12 Description",
+    },
+    {
+      id: 13,
+      src: img13,
+      alt: "Image 13",
+      caption: "Image 13 Description",
+    }
+  ];
+  
+  
+  
+export default imgSlide;

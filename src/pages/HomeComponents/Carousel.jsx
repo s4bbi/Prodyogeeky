87f9cc54834f1e8164e9ba3afa2ee103dd1427d3
vky5 <beta@blacktree.comnect.local>
2024-03-10 13:09:04 +0530

    initial Homepage

---
+++ src/pages/HomeComponents/Carousel.jsx
@@ -0,0 +1,30 @@
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import imgSlide from "../../data/slideshow";
+import "./MyCarousel.css";
+
+function MyCarousel() {
+  return (
+    <>
+      <Carousel
+        showThumbs={false}
+        showStatus={false}
+        autoPlay={true}
+        infiniteLoop={true}
+        className="carousel-container"
+      >
+        {imgSlide.map((s) => (
+          <div key={s.id} className="carousel-slide">
+            <img
+              src={s.src}
+              alt={s.alt}
+              className="object-cover w-full h-full"
+            />
+          </div>
+        ))}
+      </Carousel>
+    </>
+  );
+}
+
+export default MyCarousel;

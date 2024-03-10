5ef0770de9c67dbaa08b8811bf7de655eb29e3ee
Zeno7081205 <zenotron96@gmail.com>
2024-03-10 13:46:41 +0530

    added files

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,73 +1,77 @@
-
-import { useState } from 'react';
+import React, { useState } from 'react';
 import emailjs from '@emailjs/browser';
 
+// Define your EmailJS configuration here
+const emailjsConfig = {
+  serviceId: 'service_cba0kiv',
+  templateId: 'template_o2r247q',
+  userId: 'UcCg2U37vhYqK7rYF'
+};
+
 const Contact = () => {
-    const [isSubmitting, setIsSubmitting] = useState(false);
-    const [stateMessage, setStateMessage] = useState(null);
-    REACT_APP_EMAILJS_SERVICE_ID=service_cba0kiv
-    REACT_APP_EMAILJS_TEMPLATE_ID=template_o2r247q
-    REACT_APP_EMAILJS_USER_ID=UcCg2U37vhYqK7rYF
-    const sendEmail = (e) => {
-      e.persist();
-      e.preventDefault();
-      setIsSubmitting(true);
-      emailjs
-        .sendForm(
-            process.env.REACT_APP_EMAILJS_SERVICE_ID,
-            process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
-            e.target,
-            process.env.REACT_APP_EMAILJS_USER_ID
-        )
-        .then(
-          (result) => {
-            setStateMessage('Message sent!');
-            setIsSubmitting(false);
-            setTimeout(() => {
-              setStateMessage(null);
-            }, 5000); // hide message after 5 seconds
-          },
-          (error) => {
-            setStateMessage('Something went wrong, please try again later');
-            setIsSubmitting(false);
-            setTimeout(() => {
-              setStateMessage(null);
-            }, 5000); // hide message after 5 seconds
-          }
-        );
-      
-      // Clears the form after sending the email
-      e.target.reset();
-    };
-    return (
-        <form onSubmit={sendEmail}>
-        <div className="space-y-12">
-          <div className="border-b border-gray-900/10 pb-12">
-            <h2 className="text-base font-semibold leading-7 text-gray-900">Contact Us</h2>
-            <p className="mt-1 text-sm leading-6 text-gray-600">
-              Send us a hello . 
-            </p>
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [stateMessage, setStateMessage] = useState(null);
   
-            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 ">
-              <div className="sm:col-span-4 translate-y-6">
-                <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
-                  Name
-                </label>
-                <div className="mt-2">
-                  <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                    <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span>
-                    <input
-                      type="text"
-                      name="name"
-                      id="name"
-                      autoComplete="name"
-                      className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                      placeholder="Rohit"
-                    />
-                  </div>
+  const sendEmail = (e) => {
+    e.persist();
+    e.preventDefault();
+    setIsSubmitting(true);
+    emailjs
+      .sendForm(
+          emailjsConfig.serviceId,
+          emailjsConfig.templateId,
+          e.target,
+          emailjsConfig.userId
+      )
+      .then(
+        (result) => {
+          setStateMessage('Message sent!');
+          setIsSubmitting(false);
+          setTimeout(() => {
+            setStateMessage(null);
+          }, 5000); // hide message after 5 seconds
+        },
+        (error) => {
+          setStateMessage('Something went wrong, please try again later');
+          setIsSubmitting(false);
+          setTimeout(() => {
+            setStateMessage(null);
+          }, 5000); // hide message after 5 seconds
+        }
+      );
+    
+    // Clears the form after sending the email
+    e.target.reset();
+  };
+
+  return (
+    <form onSubmit={sendEmail}>
+      <div className="space-y-12">
+        <div className="border-b border-gray-900/10 pb-12">
+          <h2 className="text-base font-semibold leading-7 text-gray-900">Contact Us</h2>
+          <p className="mt-1 text-sm leading-6 text-gray-600">
+            Send us a hello . 
+          </p>
+
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 ">
+            <div className="sm:col-span-4 translate-y-6">
+              <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                Name
+              </label>
+              <div className="mt-2">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    autoComplete="name"
+                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    placeholder="Rohit"
+                  />
                 </div>
               </div>
-              <div className="sm:col-span-4 translate-y-6">
+            </div>
+            <div className="sm:col-span-4 translate-y-6">
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                 Email address
               </label>
@@ -81,26 +85,25 @@ const Contact = () => {
                 />
               </div>
             </div>
-  
-              <div className="col-span-full translate-y-6">
-                <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
-                  About
-                </label>
-                <div className="mt-2">
-                  <textarea
-                    id="about"
-                    name="about"
-                    rows={3}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    defaultValue={''}
-                  />
-                </div>
-                </div>
-                </div>
-                </div>
-                </div>
-                <div className="mt-6 flex items-center justify-end gap-x-6">
-        
+
+            <div className="col-span-full translate-y-6">
+              <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
+                About
+              </label>
+              <div className="mt-2">
+                <textarea
+                  id="about"
+                  name="about"
+                  rows={3}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  defaultValue={''}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-6 flex items-center justify-end gap-x-6">
         <button
           type="submit"
           className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 translate-y-6"
@@ -108,7 +111,8 @@ const Contact = () => {
           Send 
         </button>
       </div>
-                </form>
-    );
-  };
-  export default Contact;
+    </form>
+  );
+};
+
+export default Contact;

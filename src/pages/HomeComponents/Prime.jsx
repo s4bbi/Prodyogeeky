859c3b9d2b46207b8ff644c9ca7a057cdf8144f7
vky5 <beta@blacktree.comnect.local>
2024-03-10 13:38:18 +0530

    homepage

---
+++ src/pages/HomeComponents/Prime.jsx
@@ -0,0 +1,29 @@
+function Registerpage() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center ">
+        <div
+          className="mt-20 text-white lg: lg:text-9xl md:text-8xl sm:text-7xl text-7xl lg:my-3 xl:mt-20"
+          style={{ fontFamily: "nasa" }}
+        >
+          SAC
+        </div>
+        <div
+          className="justify-center my-8 text-3xl text-white lg:text-7xl md:text-6xl sm:text-5xl lg:my-5"
+          style={{ fontFamily: "nasa" }}
+        >
+          Student Activity Cell
+        </div>
+        <div
+          className="my-5 text-xl text-center text-white lg:text-6xl md:text-5xl sm:text-2xl"
+        >
+          "Feel The Pulse Of Innovation"
+        </div>
+        <div className="text-white lg:text-4xl md:text-3xl sm:text=2xl text-xl my-3">
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Registerpage;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,15 @@
+import MyCarousel from "./HomeComponents/Carousel";
+import Registerpage from "./HomeComponents/Prime";
+
 function HomePage() {
   return (
     <div>
-      <div>
       <div className="flex flex-col justify-center absolute w-full h-screen z-10">
-            <div className="">
-                <h1 className="font-samarkan text-white text-[320px] text-center">SAC</h1>
-            </div>    
-                <div className="flex justify-end pr-[186px] text-2xl">
-                    <h1 className="font-cM text-white  ">Student Activity Cell</h1>
-                </div>
-            </div>
+        <Registerpage/>
+      </div>
+
+      <div>
+        <MyCarousel/>
       </div>
     </div>
   )
